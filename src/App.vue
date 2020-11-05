e65051083398feb4de5b4b10c9d49893982cdd39
damianocasula <damiano@casula.me>
2020-11-05 20:57:13 +0100

    refactor: change menu voices

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">The game</router-link> |
+      <router-link to="/help">How to play</router-link>
     </div>
     <router-view/>
   </div>
