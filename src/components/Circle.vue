6cb4850a1910554f18b5cea31db31f1cd4b92b27
damianocasula <damiano@casula.me>
2020-11-05 22:25:35 +0100

    feat: marks SVG symbols

---
+++ src/components/Circle.vue
@@ -0,0 +1,15 @@
+<template>
+  <svg
+    width="24px"
+    height="24px"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="symbol circle"
+  >
+    <circle cx="12" cy="12" r="7"></circle>
+  </svg>
+</template>
