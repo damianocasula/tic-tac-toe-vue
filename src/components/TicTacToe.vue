7670279a7b7c4d6251f7b80787db899cdfc30c4a
damianocasula <damiano@casula.me>
2020-11-05 20:59:41 +0100

    feat: design tic-tac-toe board

---
+++ src/components/TicTacToe.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="tic-tac-toe">
+    <div class="cell">
+      <div class="cross"></div>
+    </div>
+    <div class="cell">
+      <div class="circle"></div>
+    </div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TicTacToe',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style lang="scss">
+@import "@/assets/variables.scss";
+
+.tic-tac-toe {
+  display: grid;
+  grid-template-columns: repeat(3, $cell-size);
+  grid-template-rows: repeat(3, $cell-size);
+  grid-gap: $small-spacing;
+  background-color: black;
+
+  .cell {
+    background-color: white;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="game-container">
+      <div class="game-board">
+        <TicTacToe msg="Welcome to Your Vue.js App" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import TicTacToe from '@/components/TicTacToe.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    TicTacToe
   }
 }
 </script>
+
+<style lang="scss">
+.game-container {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+
+  .game-board {
+    display: flex;
+  }
+}
+</style>
