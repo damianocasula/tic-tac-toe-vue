f4505e1c7942e58080183c96aed541d3fef063f5
damianocasula <damiano@casula.me>
2020-11-05 22:26:10 +0100

    feat: improve game component

--- src/components/TicTacToe.vue
+++ src/components/TicTacToe.vue
@@ -1,24 +1,31 @@
 <template>
   <div class="tic-tac-toe">
-    <div class="cell">
-      <div class="cross"></div>
+    <div class="cell" v-for="value in [].concat.apply([], board)" :key="value">
+      <CrossSymbol v-show="value == 1" />
+      <CircleSymbol v-show="value == 2" />
     </div>
-    <div class="cell">
-      <div class="circle"></div>
-    </div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
   </div>
 </template>
 
 <script>
+import CrossSymbol from '@/components/Cross'
+import CircleSymbol from '@/components/Circle'
+
 export default {
   name: 'TicTacToe',
+  data () {
+    return {
+      board: [
+        [1, 2, 0],
+        [0, 1, 2],
+        [2, 0, 0]
+      ]
+    }
+  },
+  components: {
+    CrossSymbol,
+    CircleSymbol
+  },
   props: {
     msg: String
   }
@@ -33,10 +40,28 @@ export default {
   grid-template-columns: repeat(3, $cell-size);
   grid-template-rows: repeat(3, $cell-size);
   grid-gap: $small-spacing;
-  background-color: black;
+  background-color: $main-color;
 
   .cell {
-    background-color: white;
+    background-color: white; /* TODO: make it a variable */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .symbol {
+      color: $main-color;
+      width: 100%;
+      height: 100%;
+
+      &.cross {
+        color: $accent-color;
+      }
+    }
+
+    &:hover {
+      background-color: $light-main-color;
+      /* TODO: when hovering, display a low-opacity version of the symbol of the player in the turn */
+    }
   }
 }
 </style>
