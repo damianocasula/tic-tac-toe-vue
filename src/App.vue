237ec473760db98992a4f9d5a7665bb49f8b3a7b
damianocasula <damiano@casula.me>
2020-11-05 20:58:18 +0100

    feat: use variables

--- src/App.vue
+++ src/App.vue
@@ -9,23 +9,24 @@
 </template>
 
 <style lang="scss">
+
+@import "@/assets/variables.scss";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: $main-color;
 }
 
 #nav {
-  padding: 30px;
+  padding: $normal-spacing;
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: $main-color;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: $accent-color;
     }
   }
 }
