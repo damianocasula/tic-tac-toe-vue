0a66e72e412353482eae1ffb6248d458ba028e1f
damianocasula <damiano@casula.me>
2020-11-14 22:30:24 +0100

    checkpoint: prepare for implementing the dark mode toggle

---
+++ src/components/ModeToggle.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="mode-toggle">
+    <circle-icon size="1.5x" class="circle-icon"></circle-icon> 
+  </div>
+</template>
+
+<script>
+import { CircleIcon } from 'vue-feather-icons'
+
+export default {
+  name: 'Mode Toggle',
+  components: {
+    CircleIcon
+  }
+}
+</script>
+
+<style>
+
+</style>
