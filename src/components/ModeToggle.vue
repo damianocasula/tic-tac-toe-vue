3333d0711b95daf9933300182e75a9bfa2a15aac
damianocasula <damiano@casula.me>
2020-11-17 14:10:39 +0100

    style: css for dark mode toggle

--- src/components/ModeToggle.vue
+++ src/components/ModeToggle.vue
@@ -15,6 +15,25 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "@/assets/variables.scss";
 
+.mode-toggle {
+  padding: 5px 10px 0;
+
+  &:hover {
+    background-color: #f7f7f7;
+    cursor: pointer;
+    border-radius: 5px;
+  }
+
+  &:active {
+    color: $secondary-color;
+  }
+
+  svg {
+    width: 16px;
+    stroke-width: 2.4px;
+  }
+}
 </style>
